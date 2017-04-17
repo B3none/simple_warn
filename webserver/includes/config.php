2433c05cee9e915b978a1e2882f2d43d86a1e50c
b3none <ablackham2000@gmail.com>
2017-04-17 23:58:31 +0100

    Create config.php

---
+++ webserver/includes/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$db = ""; // The IP address of your MySQL database.
+$db_user = ""; // The user of the database.
+$db_password = ""; // The password of the user.
+$db_name = "warnings"; // The name of the database (Default = warnings).
+$db_table = "warnings"; // The table of the database.
+
+$connect = mysql_connect()
+
+/*
+* TODO
+* - Connect to db.
+* - Select db table.
+*/
+
+?>
