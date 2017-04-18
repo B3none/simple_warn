6ded8993f4207b3fa6d350d0c15280b3b641ce1c
b3none <ablackham2000@gmail.com>
2017-04-18 01:43:06 +0100

    Update index.php

--- webserver/install/index.php
+++ webserver/install/index.php
@@ -3,14 +3,34 @@ include('includes/config.php');
 
 if($connect === true)
 {
-	$creation_query = 'CREATE TABLE IF NOT EXISTS `warnings`.`warnings` ( `warningid` INT NOT NULL AUTO_INCREMENT , `warningtype` TEXT NOT NULL , `server` TEXT NOT NULL , `date` TEXT NOT NULL , `client` TEXT NOT NULL , `reason` TEXT NOT NULL , `admin` TEXT NOT NULL , PRIMARY KEY (`warningid`))';
+	$creation_query = '
+	SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+	SET time_zone = "+00:00";
+
+	CREATE TABLE `warnings` (
+	  `warningid` int(11) NOT NULL,
+	  `warningtype` text NOT NULL,
+	  `server` text NOT NULL,
+	  `date` text NOT NULL,
+	  `client` text NOT NULL,
+	  `client_steamid` text NOT NULL,
+	  `reason` text NOT NULL,
+	  `admin` text NOT NULL,
+	  `admin_steamid` text NOT NULL
+	) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+	ALTER TABLE `warnings` ADD PRIMARY KEY (`warningid`);
+	  
+	ALTER TABLE `warnings` MODIFY `warningid` int(11) NOT NULL AUTO_INCREMENT;';
+	
 	if(mysql_query($creation_query) == true)
 	{
 		echo'<h1>Sweet!</h1>
 		<br>
 		<p>Your table creation was successfull!</p>
 		<br>
-		<p>Simply delete the "install" directory to progress (<b><u>This is for security reasons!</b></u>)</p>';
+		<p>Simply delete the "install" directory to progress (<b><u>This is for security reasons!</b></u>)</p>
+		';
 	}
 }
 else
