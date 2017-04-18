774d97f7f53576759a1a383828c28edd85864350
b3none <ablackham2000@gmail.com>
2017-04-18 01:45:52 +0100

    Update config.php

--- webserver/includes/config.php
+++ webserver/includes/config.php
@@ -3,8 +3,7 @@
 $db_host = "localhost"; // The IP address of your MySQL database.
 $db_user = "root"; // The user of the database.
 $db_password = ""; // The password of the user.
-$db = "warnings"; // The name of the database (Default = warnings).
-$db_table = "warnings"; // The table of the database.
+$db_table = "warnings"; // The table of the database (Default = warnings).
 
 $connect_initial = mysql_connect($db_host, $db_user, $db_password);
 $connect = mysql_select_db($db_table, $connect_initial);
