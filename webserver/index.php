7d9ed0c729f713c639680e981d7146f87328c431
b3none <ablackham2000@gmail.com>
2017-04-18 02:26:36 +0100

    Update index.php

--- webserver/index.php
+++ webserver/index.php
@@ -11,47 +11,51 @@
         }
         </style>
     </head>
-<body>
+    <body>
+	<?php
+	/*
+	* Code by B3none -> http://steamcommunity.com/profiles/76561198028510846
+	*/
 
-<?php
-/*
-* Code by B3none -> http://steamcommunity.com/profiles/76561198028510846
-*/
+	include('includes/config.php');
+	echo"<title>$page_title</title>";
 
-include('includes/config.php');
-echo"<title>$page_title</title>";
+	$order_query = "SELECT * FROM `$db_table` ORDER BY `date`";
+	$connect_and_order = mysql_query($order_query, $connect);
 
-$order_query = "SELECT * FROM `$db_table` ORDER BY `date`";
-$connect_and_order = mysql_query($order_query, $connect);
+	echo "
+	<table id='warnings'>
+	    <tr>
+		<th>Warning ID</th>
+		<th>Warning Type</th>
+		<th>Server</th>
+		<th>Client</th>
+		<th>Admin</th>
+		<th>Date</th>
+	    </tr>
+	</td>";
 
-echo "
-<table id='warnings'>
-    <tr>
-        <th>Warning ID</th>
-        <th>Warning Type</th>
-        <th>Server</th>
-        <th>Client</th>
-        <th>Admin</th>
-        <th>Date</th>
-    </tr>
-</td>";
+	$id = 0;
+	while($row = mysql_fetch_array($connect_and_order))
+	{
+	    $id++;
+		echo"
+		<tr>
+			<td>$id</td>
+			<td>$row['warningtype']</td>
+			<td>$row['server']</td>
+			<td>$row['client']</td>
+			<td>$row['admin']</td>
+			<td>$row['date']</td>
+		</tr>
+		";
+	}
+	echo"</table>";
 
-$id = 0;
-while($row = mysql_fetch_array($connect_and_order))
-{
-    $id++;
-	echo"
-	<tr>
-		<td>$id</td>
-		<td>$row['warningtype']</td>
-		<td>$row['server']</td>
-		<td>$row['client']</td>
-		<td>$row['admin']</td>
-		<td>$row['date']</td>
-	</tr>
-	";
-}
-echo"</table>";
-	
-mysql_close($connect_and_order);
-?>
+	mysql_close($connect_and_order);
+	?>
+    </body>
+    <footer>
+	<p>Coded by B3none.</p>
+    </footer>
+</html>
